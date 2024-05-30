1784163bcbe92a33c0e879fae244ea6384aa6fcb
Lê Minh Hưng <85102936+HungLM1506@users.noreply.github.com>
2024-05-30 15:30:19 +0700

    Update utils.py

--- src/gaussian_core/utils.py
+++ src/gaussian_core/utils.py
@@ -128,7 +128,7 @@ def training(opt, dataloader, gaussians, use_colmap=None):
     else:
         pts_from_depth, color = init_point(
             'COLON_CUSTOM/depth/frame_0_depth.png', 'COLON_CUSTOM/images/frame_0.png', 'COLON_CUSTOM/poses_bounds.npy')
-        normals = np.zeros_like(xyz)
+        normals = np.zeros_like(pts_from_depth)
         pcd = BasicPointCloud(points=pts_from_depth,
                               colors=color, normals=normals)
     gaussians.create_from_pcd(pcd, spatial_lr_scale)
