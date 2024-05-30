e714369daa001f075487f26cd44264da500d9696
Minh Hung <leminhhung933@gmail.com>
2024-05-31 00:17:04 +0700

    utils

--- src/gaussian_core/utils.py
+++ src/gaussian_core/utils.py
@@ -20,7 +20,18 @@ from gaussian_core.cameras import convert_gs_to_pytorch3d
 from pytorch3d.transforms import quaternion_apply, quaternion_invert
 from utils.loss_utils import ssim
 from gaussian_core.init_point_from_depth import init_point
-from pre_process.remove_outlier_pcd import remove_noise_pts
+
+import open3d as o3d
+
+
+def remove_noise_pts(path_point):
+
+    pcd = o3d.io.read_point_cloud(path_point)
+    cl, ind = pcd.remove_statistical_outlier(nb_neighbors=50, std_ratio=0.5)
+    inlier_cloud = pcd.select_by_index(ind)
+    # o3d.io.write_point_cloud(
+    #     "filtered_point_cloud_base.ply", inlier_cloud)
+    return inlier_cloud
 
 
 def to8b(x): return (255*np.clip(x.cpu().numpy(), 0, 1)).astype(np.uint8)
