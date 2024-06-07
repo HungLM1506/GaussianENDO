a31da229e822740a23f1f89db2eb884c5e08a300
Minh Hung <leminhhung933@gmail.com>
2024-06-07 17:07:01 +0700

    init_point

--- src/gaussian_core/init_point_from_depth.py
+++ src/gaussian_core/init_point_from_depth.py
@@ -17,7 +17,7 @@ def load_pose(path_poses):
     poses = np.concatenate(
         [poses[..., :1], -poses[..., 1:2], -poses[..., 2:3], poses[..., 3:4]], -1)
     image_poses = []
-    image_times = []
+    #
     for idx in range(poses.shape[0]):
         pose = poses[idx]
         c2w = np.concatenate((pose, np.array([[0, 0, 0, 1]])), axis=0)  # 4x4
@@ -26,29 +26,30 @@ def load_pose(path_poses):
         T = w2c[:3, -1]
         R = np.transpose(R)
         image_poses.append((R, T))
-        image_times.append(idx / poses.shape[0])
+        # image_times.append(idx / poses.shape[0])
     return image_poses
 
 
-def get_pts_wld(pts_cams, poses):
+def get_pts_wld(pts_cams, poses, interval=1):
     pts_wlds = []
-    for pose in poses:
+    for i, pose in enumerate(poses):
         R, T = pose
         R = np.transpose(R)
         w2c = np.concatenate((R, T[..., None]), axis=-1)
         w2c = np.concatenate((w2c, np.array([[0, 0, 0, 1]])), axis=0)
         c2w = np.linalg.inv(w2c)
         pts_cam_homo = np.concatenate(
-            (pts_cams, np.ones((pts_cams.shape[0], 1))), axis=-1)
+            (pts_cams[i], np.ones((pts_cams[i].shape[0], 1))), axis=-1)
         pts_wld = np.transpose(c2w @ np.transpose(pts_cam_homo))
         pts_wld = pts_wld[:, :3]
-        pts_wlds.append(pts_wld)
+        if i % interval == 0:
+            pts_wlds.append(pts_wld)
     return np.array(pts_wlds).reshape(-1, 3)
 
 
 def get_color_depth(depth_dir, image_dir):
     depths = []
-    for depth_path in os.listdir(depth_dir):
+    for i, depth_path in enumerate(os.listdir(depth_dir)):
         depth = np.array(Image.open(os.path.join(
             depth_dir, depth_path)))[..., 0] / 255.0
         depth[depth != 0] = (1 / depth[depth != 0])*0.4
@@ -56,7 +57,7 @@ def get_color_depth(depth_dir, image_dir):
         depths.append(depth)
 
     colors = []
-    for img_path in os.listdir(image_dir):
+    for j, img_path in enumerate(os.listdir(image_dir)):
         color = np.array(Image.open(os.path.join(
             image_dir, img_path))) / 255.0
         colors.append(color)
@@ -76,8 +77,8 @@ def get_pts_cam(depths, colors):
         X = X_Z * Z
         Y = Y_Z * Z
         pts_cam = np.stack((X, Y, Z), axis=-1).reshape(-1, 3)
-        pts_cams.append(pts_cam)
         color = colors[index]
+        pts_cams.append(pts_cam)
         colors_cams.append(color)
     return pts_cams, np.array(colors_cams).reshape(-1, 3)
 
@@ -90,7 +91,7 @@ def remove_noise_pts_with_color(point_cloud_np, color_np):
 
     # Remove statistical outliers
     cl, ind = pcd.remove_statistical_outlier(
-        nb_neighbors=16, std_ratio=0.003)
+        nb_neighbors=50, std_ratio=0.02)
     inlier_cloud = pcd.select_by_index(ind)
 
     # Convert back to numpy arrays
@@ -104,10 +105,29 @@ def init_point(depth_dir, img_dir, pose_path):
     poses = load_pose(pose_path)
     depths, colors = get_color_depth(depth_dir, img_dir)
     pts_cam, color_cam = get_pts_cam(depths=depths, colors=colors)
-    pts = get_pts_wld(pts_cam, poses)
+    pts = get_pts_wld(pts_cam, poses, interval=6)
     pts_final, color_final = remove_noise_pts_with_color(pts, color_cam)
+    print(len(pts_final))
     return pts_final, color_final
 
 
-pts_final, color_final = init_point(
-    'pre_process/depth', 'pre_process/images', 'data_test/poses_bounds.npy')
+# pts_final, color_final = init_point(
+#     'pre_process/depth', 'pre_process/images', 'data_test/poses_bounds.npy')
+
+
+# def visualize(points):
+#     import numpy as np
+#     import open3d as o3d
+
+#     point_cloud = o3d.geometry.PointCloud()
+#     point_cloud.points = o3d.utility.Vector3dVector(points)
+
+#     vis = o3d.visualization.Visualizer()
+#     vis.create_window()
+#     vis.add_geometry(point_cloud)
+
+#     vis.run()
+#     vis.destroy_window()
+
+
+# visualize(pts_final)
